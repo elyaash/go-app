d0eb8300bbc4c5f7a624636758dec38b63525385
Prakash <prajput@pch.com>
2023-08-21 15:24:25 -0400

    Added logging

--- main.go
+++ main.go
@@ -4,13 +4,19 @@ import (
 	"elyaash/auth"
 	"elyaash/auth/config"
 	"elyaash/auth/handlers"
+	"fmt"
+	"os"
+	"time"
+
+	myLog "log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
 )
 
 func main() {
-
+	logger := getLogger("system")
+	logger.Println("Starting web server...")
 	app := fiber.New()
 
 	authApi := app.Group("/auth")
@@ -28,8 +34,20 @@ func main() {
 		return c.SendString("Welcome to Go App")
 	})
 
-	err := app.Listen(":80")
+	err := app.Listen(":3000")
 	if err != nil {
 		log.Error(err)
 	}
+	logger.Println("Error occured...", err)
+}
+
+func getLogger(name string) *myLog.Logger {
+	year, month, day := time.Now().Date()
+	LOG_FILE_LOCATION := fmt.Sprintf("./logs/"+name+"-%d-%d-%dd.log", month, day, year)
+	file, err := os.OpenFile(LOG_FILE_LOCATION, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	if err != nil {
+		//log.Error("Could not create a log file error\n", err)
+		panic(err)
+	}
+	return myLog.New(file, "", myLog.Ldate|myLog.Ltime|myLog.Lshortfile)
 }
